4ff9f30cd95560abc30bb7143ca1250eb9d16289
snekw <dev.snekw@gmail.com>
2019-04-13 22:45:11 +0300

    Collision testing, fix orb generation, game over state.

--- src/apps/snake/index.ts
+++ src/apps/snake/index.ts
@@ -47,7 +47,8 @@ interface IGameState {
   lastInput: action,
   lastDirection: ICoordinate,
   lastWasOrb: boolean,
-  orb?: ICoordinate
+  orb?: ICoordinate,
+  gameOver: boolean
 }
 /**
  * Snake
@@ -84,15 +85,24 @@ const keybindings: KeyBinding[] = [
 ]
 
 const headPos: ICoordinate = { x: Math.round(gridWidth / 2), y: Math.round(gridWidth / 2) }
-const gameState: IGameState = {
-  score: 0,
-  snake: {
-    headLocation: { x: headPos.x, y: headPos.y },
-    locations: [{ x: headPos.x, y: headPos.y }, { x: headPos.x - 1, y: headPos.y }, { x: headPos.x - 2, y: headPos.y }]
-  },
-  lastInput: action.RIGHT,
-  lastDirection: direction.RIGHT,
-  lastWasOrb: false
+let gameState: IGameState = initGameState()
+
+function initGameState (): IGameState {
+  return {
+    score: 0,
+    snake: {
+      headLocation: { x: headPos.x, y: headPos.y },
+      locations: [
+        { x: headPos.x, y: headPos.y },
+        { x: headPos.x - 1, y: headPos.y },
+        { x: headPos.x - 2, y: headPos.y }
+      ]
+    },
+    lastInput: action.RIGHT,
+    lastDirection: direction.RIGHT,
+    lastWasOrb: false,
+    gameOver: false
+  }
 }
 
 /***************** RENDERING **************/
@@ -117,12 +127,31 @@ function render (delta: number) {
 
   // draw score
   ctx.fillStyle = '#fff'
+  ctx.textAlign = 'left'
+  ctx.textBaseline = 'middle'
   ctx.font = '40px monospace'
   ctx.fillText(gameState.score.toString(), 5, 30)
+
+  if (gameState.gameOver) {
+    // highlight collision
+    drawSnakePart(gameState.snake.headLocation, '#fca80c')
+
+    // shadow
+    ctx.fillStyle = '#2a2a2ab0'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    // text
+    ctx.fillStyle = '#f00'
+    ctx.font = '80px monospace'
+    const gameOverText = 'Game Over!'
+    ctx.textAlign = 'center'
+    ctx.textBaseline = 'middle'
+    ctx.fillText(gameOverText, canvas.width / 2, canvas.height / 2)
+  }
 }
 
-function drawSnakePart (coord: ICoordinate) {
-  ctx.fillStyle = '#f00'
+function drawSnakePart (coord: ICoordinate, style: string = '#f00') {
+  ctx.fillStyle = style
   const adjX = coord.x * pixelSize + pixelSize * 0.01
   const adjY = coord.y * pixelSize + pixelSize * 0.01
   ctx.fillRect(adjX, adjY, pixelSize * 0.98, pixelSize * 0.98)
@@ -132,7 +161,44 @@ function drawSnakePart (coord: ICoordinate) {
 function update (delta: number) {
   DEBUG && log('Last action: ', gameState.lastInput)
 
-  // movement
+  if (gameState.gameOver) {
+    // allow restart on action
+    if (gameState.lastInput === action.ACTION) {
+      gameState = initGameState()
+    }
+    return
+  }
+
+  move()
+
+  orb()
+
+  collision()
+}
+
+function testSnakeCollision (coord: ICoordinate, skipHead: boolean = false): boolean {
+  return !gameState.snake.locations.every(
+    (v, i) => ((skipHead === true && i === 0) || !(v.x === coord.x && v.y === coord.y))
+  )
+}
+
+function collision () {
+  const head = gameState.snake.headLocation
+  // self collsion
+  if (testSnakeCollision(head, true)) {
+    gameState.gameOver = true
+  }
+
+  // out of bounds
+  if (head.x < 0 || head.y < 0 || head.x >= gridWidth || head.y >= gridWidth) {
+    gameState.gameOver = true
+  }
+  if (gameState.gameOver) {
+    gameState.lastInput = action.RIGHT
+  }
+}
+
+function move () {
   let currDirection: ICoordinate = gameState.lastDirection
   switch (gameState.lastInput) {
     case action.UP:
@@ -167,11 +233,12 @@ function update (delta: number) {
   }
   gameState.lastWasOrb = false
   gameState.snake.headLocation = newLocation
+}
 
-  // orb
+function orb () {
   if (gameState.orb) {
     if (gameState.orb.x === gameState.snake.headLocation.x && gameState.orb.y === gameState.snake.headLocation.y) {
-      gameState.score += 10
+      gameState.score += 1
       gameState.orb = generateOrb()
       gameState.lastWasOrb = true
     }
@@ -179,8 +246,13 @@ function update (delta: number) {
 }
 
 function generateOrb (): ICoordinate {
-  const rx = Math.round(Math.random() * gridWidth)
-  const ry = Math.round(Math.random() * gridWidth)
+  let rx
+  let ry
+
+  do {
+    rx = Math.trunc(Math.random() * (gridWidth))
+    ry = Math.trunc(Math.random() * (gridWidth))
+  } while (testSnakeCollision({ x: rx,y: ry }))
   return { x: rx, y: ry }
 }
 
@@ -206,11 +278,14 @@ function handleKeyboard (event: KeyboardEvent) {
 
 let lastFrameTimestamp: number = 0
 let updateDelta: number = 0
-const updateThreshold = 1000
+let updateThreshold = 900
 function gameLoop (timestamp: number) {
   const delta: number = timestamp - lastFrameTimestamp
 
   updateDelta += delta
+
+  updateThreshold = 1000 - gameState.score * 25
+  updateThreshold = updateThreshold < 5 || gameState.gameOver ? 5 : updateThreshold
 
   if (updateDelta > updateThreshold) {
     update(delta)
