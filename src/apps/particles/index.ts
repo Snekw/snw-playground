3a3a832092d8e0dc4177e2073af97f327f81a218
snekw <dev.snekw@gmail.com>
2019-08-13 20:43:21 +0300

    Simplyfy particle demo draw calls. Make it cooler

--- src/apps/particles/index.ts
+++ src/apps/particles/index.ts
@@ -1,9 +1,9 @@
 import { ParticleSystem } from 'apps/particles/particle'
 import 'styles/app.scss'
 
+import { IVec2 } from 'lib/math'
 import basicParticleFrag from './shaders/basicParticle.frag'
 import basicParticleTransform from './shaders/basicParticle.transform'
-import basicParticleVert from './shaders/basicParticle.vert'
 // create canvas
 /**
  * The canvas used for rendering
@@ -28,30 +28,35 @@ resize()
 // bind the window resize to the resize function
 window.onresize = resize
 
-const particleSystem = new ParticleSystem(gl, basicParticleTransform, basicParticleVert, basicParticleFrag)
+const particleSystem = new ParticleSystem(gl, basicParticleTransform, basicParticleFrag)
 
-const particle = particleSystem.generate(100000)
+const particle = particleSystem.generate(200000)
 
-const draw = () => {
+let mouseLoc: IVec2 = { x: 0, y: 0 }
+let repel: boolean = false
+
+canvas.addEventListener('mousemove', (ev: MouseEvent) => {
+  const x = (ev.clientX / window.innerWidth * 2) - 1
+  const y = (ev.clientY / window.innerHeight * -2) + 1
+  mouseLoc = { x, y }
+})
+
+canvas.addEventListener('mouseout', () => {
+  mouseLoc = { x: 0, y: 0 }
+})
+
+canvas.addEventListener('mousedown', () => repel = true)
+canvas.addEventListener('mouseup', () => repel = false)
+canvas.addEventListener('mouseout', () => repel = false)
+
+let nUpdates = -1
+const autoUpdate = () => {
+
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
   gl.clearColor(0.2, 0.2, 0.2, 1)
   gl.clear(gl.COLOR_BUFFER_BIT)
 
-  particle.draw()
-}
-
-const update = () => {
-  particle.update()
-}
-
-let lastUpdate = 0
-let nUpdates = -1
-const autoUpdate = (timestamp: number) => {
-  const delta = lastUpdate - timestamp
-  lastUpdate = timestamp
-
-  update()
-  draw()
+  particle.update(mouseLoc, repel)
 
   if (nUpdates > 0 || nUpdates === -1) {
     window.requestAnimationFrame(autoUpdate)
--- src/apps/particles/particle.ts
+++ src/apps/particles/particle.ts
@@ -1,9 +1,9 @@
+import { IVec2 } from 'lib/math'
 import {
   createBuffer,
   createProgram2,
   createVAO
 } from 'lib/renderUtil'
-import blankFragmentShader from './shaders/blank.frag'
 
 const VERTEX_ATTRIB_POS = 0
 const VELOCITY_ATTRIB_POS = 1
@@ -11,12 +11,10 @@ const VELOCITY_ATTRIB_POS = 1
 export class ParticleSystem {
   private readonly gl: WebGL2RenderingContext
   private readonly transformProgram: WebGLProgram
-  private readonly drawProgram: WebGLProgram
 
   public constructor (
     gl: WebGL2RenderingContext,
     transformShaderStr: string,
-    vertexShaderStr: string,
     fragmentShader: string
   ) {
     this.gl = gl
@@ -24,20 +22,14 @@ export class ParticleSystem {
     this.transformProgram = createProgram2(
       gl,
       transformShaderStr,
-      blankFragmentShader,
+      fragmentShader,
       ['v_position', 'v_velocity'],
       this.gl.SEPARATE_ATTRIBS
-    )
-
-    this.drawProgram = createProgram2(
-      gl,
-      vertexShaderStr,
-      fragmentShader
     )
   }
 
   public generate (nParticles: number): Particle {
-    return new Particle(this.gl, this.transformProgram, this.drawProgram, nParticles)
+    return new Particle(this.gl, this.transformProgram, nParticles)
   }
 }
 
@@ -50,8 +42,8 @@ export class Particle {
     const particleArr = new Float32Array(particleArrLength)
     let i = particleArrLength - 1
     for (; i > 0; i -= 2) {
-      particleArr[i] = 0.5 - Math.random()
-      particleArr[i - 1] = 0.5 - Math.random()
+      particleArr[i] = (0.5 - Math.random()) * 2
+      particleArr[i - 1] = (0.5 - Math.random()) * 2
     }
 
     return [
@@ -59,48 +51,50 @@ export class Particle {
       createBuffer(gl, particleArrLength, gl.DYNAMIC_COPY)
     ]
   }
+
   private static generateVelocityBuffers (gl: WebGL2RenderingContext, nParticles: number): WebGLBuffer[] {
-    return Particle.generateVertexBuffers(gl, nParticles)
+    const particleArrLength = nParticles * 2 * 4
+    // particle arr format:
+    const particleArr = new Float32Array(particleArrLength)
+    let i = particleArrLength - 1
+    for (; i > 0; i -= 2) {
+      particleArr[i] = 0
+      particleArr[i - 1] = 0
+    }
+
+    return [
+      createBuffer(gl, particleArr, gl.DYNAMIC_COPY),
+      createBuffer(gl, particleArrLength, gl.DYNAMIC_COPY)
+    ]
   }
-
-  // private static createVertexArrays (gl: WebGL2RenderingContext, vertexBuffer) {
-  //   const vertexBuffer = new Float32Array(100)
-
-  //   const va1 = createVAO([{
-
-  //   }])
-
-  //   const va2 = gl.createVertexArray()
-
-  //   return [va1, va2]
-  // }
 
   private readonly gl: WebGL2RenderingContext
   private readonly transformProgram: WebGLProgram
-  private readonly drawProgram: WebGLProgram
   private readonly nParticles: number
   private readonly vertexBuffers: WebGLBuffer[]
   private readonly velocityBuffers: WebGLBuffer[]
   private readonly fb: WebGLTransformFeedback
   private readonly feedBackVAOs: WebGLVertexArrayObject[]
-  private readonly drawVAOs: WebGLVertexArrayObject[]
+  private readonly uMouseLocation: WebGLUniformLocation
+  private readonly uRepel: WebGLUniformLocation
   private currentIndex: number = 0
 
   public constructor (
     gl: WebGL2RenderingContext,
     transformProgram: WebGLProgram,
-    drawProgram: WebGLProgram,
     nParticles: number
   ) {
     this.gl = gl
     this.transformProgram = transformProgram
-    this.drawProgram = drawProgram
 
     this.nParticles = nParticles
 
     this.vertexBuffers = Particle.generateVertexBuffers(this.gl, this.nParticles)
     this.velocityBuffers = Particle.generateVelocityBuffers(this.gl, this.nParticles)
     this.fb = this.gl.createTransformFeedback()
+
+    this.uMouseLocation = this.gl.getUniformLocation(this.transformProgram, 'u_mouse_location')
+    this.uRepel = this.gl.getUniformLocation(this.transformProgram, 'u_repel')
 
     this.feedBackVAOs = [
       createVAO(this.gl, [
@@ -128,30 +122,10 @@ export class Particle {
         }
       ])
     ]
-
-    this.drawVAOs = [
-      createVAO(this.gl, [
-        {
-          buffer: this.vertexBuffers[0],
-          location: VERTEX_ATTRIB_POS,
-          elementSize: 2
-        }
-      ]),
-      createVAO(this.gl, [
-        {
-          buffer: this.vertexBuffers[1],
-          location: VERTEX_ATTRIB_POS,
-          elementSize: 2
-        }
-      ])
-    ]
   }
 
-  public update () {
-
+  public update (mousePos: IVec2, repel: boolean) {
     const invertedIndex = this.currentIndex === 1 ? 0 : 1
-
-    this.gl.enable(this.gl.RASTERIZER_DISCARD)
 
     this.gl.bindTransformFeedback(this.gl.TRANSFORM_FEEDBACK, this.fb)
 
@@ -160,25 +134,18 @@ export class Particle {
 
     this.gl.useProgram(this.transformProgram)
 
+    this.gl.uniform2f(this.uMouseLocation, mousePos.x, mousePos.y)
+    this.gl.uniform1f(this.uRepel, repel ? 1.0 : 0.0)
+
     this.gl.beginTransformFeedback(this.gl.POINTS)
     this.gl.bindVertexArray(this.feedBackVAOs[this.currentIndex])
     this.gl.drawArrays(this.gl.POINTS, 0, this.nParticles)
     this.gl.endTransformFeedback()
 
-    this.gl.disable(this.gl.RASTERIZER_DISCARD)
-
     this.gl.bindBufferBase(this.gl.TRANSFORM_FEEDBACK_BUFFER, 0, null)
     this.gl.bindBufferBase(this.gl.TRANSFORM_FEEDBACK_BUFFER, 1, null)
     this.gl.bindTransformFeedback(this.gl.TRANSFORM_FEEDBACK, null)
-  }
 
-  public draw () {
-    const invertedIndex = this.currentIndex === 1 ? 0 : 1
-
-    this.gl.useProgram(this.drawProgram)
-    this.gl.bindVertexArray(this.drawVAOs[invertedIndex])
-    this.gl.drawArrays(this.gl.POINTS, 0, this.nParticles)
-    this.gl.bindVertexArray(null)
     this.currentIndex = invertedIndex
   }
 }
